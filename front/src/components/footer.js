d2ed550cff101960a751228fb870a6a8d9ecda3e
JUAN SEBASTIAN ESPITIA ACERO <js.espitia@uniandes.edu.co>
2019-03-27 21:59:07 -0500

    Separacion footer header

---
+++ front/src/components/footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class Footer extends Component{
+    render(){
+        return(
+            <div className="copy-right"> 
+                <div className="container">
+                    <p>© 2016 Smart bazaar . All rights reserved | Design by <a href="http://w3layouts.com"> W3layouts.</a></p>
+                </div>
+            </div>
+        );
+    }
+}
