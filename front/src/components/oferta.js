30120b765177d65da1c71a23e2ec492d0c9d829e
Jorleidis Monsalvo <jp.monsalvo@uniandes.edu.co>
2019-03-29 01:38:23 -0500

    crear Prestamo

--- front/src/components/oferta.js
+++ front/src/components/oferta.js
@@ -1,25 +1,64 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 
 export class oferta extends Component {
   constructor(props){
     super(props);
     this.state = {
-      ofertas : []
+      ofertas : [],
+      usuario: JSON.parse(localStorage.getItem("user"))
     };
   }
-
+  
   componentDidMount = () => {
-    fetch('http://localhost:3001/users/:userID/ofertas')
-    .then(function(res) {
-      this.setState({ofertas : res.data})
+
+    console.log(JSON.parse(localStorage.getItem("user")));
+    
+    fetch(`http://localhost:3001/users/b1d308ad-5770-4d13-ac1e-8c8fc107dad4/ofertas`)
+    .then(response => response.json())
+    .then(ofertas => {
+      ofertas.forEach(oferta => {
+        let data = {
+          id: oferta.id,
+          nombre: oferta.nombre,
+          img: oferta.img,
+          precioAnterior: oferta.precioAnterior,
+          porcentajeDescuento: oferta.porcentajeDescuento,
+          userId: oferta.userId,
+          objetosId: oferta.objetosId
+        }
+        this.setState({ ofertas: this.state.ofertas.concat([data]) })
+      })
     })
   }
-    
+
   render() {
-      const {  } = this.props
+    const {ofertas } = this.state
     return (
       <div>
-        
+      <h1>Prestamos usuario {this.state.usuario.name}</h1>
+      <div className="container">
+            <div className="row">
+        {ofertas.map((prestamo,index)=>(
+          <Fragment>
+            <div className="col-auto mb-3">
+            <div className="card" style={{width: "25rem"}}>
+              <div className="card-body">
+                <h2 className="card-title">Prestamo {prestamo.id} </h2>
+                  <p className="prestamoT" style={{fontSize: "1.8rem"}}>
+                  Id pago: {prestamo.paymentId} <br></br>
+                  Id objeto: {prestamo.objectId} <br></br>
+                  Fecha inicio: {prestamo.startDate} <br></br>
+                  Fecha fin: {prestamo.endDate} <br></br>
+                  <a href="#" className="card-link">Actualizar</a>
+                  <a href="#" className="card-link">Eliminar</a>
+                  </p>
+            </div>
+            </div>
+            </div>
+           </Fragment>
+        ))}
+        </div>
+        </div>
       </div>
     )
   }
--- front/src/components/prestamo.js
+++ front/src/components/prestamo.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component,Fragment } from 'react'
+import {Link, Route} from 'react-router-dom'
+import CrearPrestamo from './crearPrestamo'
 
 export class prestamo extends Component {
 
@@ -6,41 +8,70 @@ export class prestamo extends Component {
     super(props);
     this.state = {
       prestamos : [],
-      usuario: {},
+      usuario: JSON.parse(localStorage.getItem("user"))
     };
   }
-
+  
   componentDidMount = () => {
-    fetch('http://localhost:3001/users/:userID/prestamos')
-    .then(function(res) {
-      this.setState({prestamos : res.data})
+
+    console.log(JSON.parse(localStorage.getItem("user")));
+    
+    fetch(`http://localhost:3001/users/b1d308ad-5770-4d13-ac1e-8c8fc107dad4/prestamos`)
+    .then(response => response.json())
+    .then(prestamos => {
+      prestamos.forEach(prestamo => {
+        let data = {
+          id: prestamo.id,
+          paymentId: prestamo.paymentId,
+          objectId: prestamo.objectId,
+          startDate: prestamo.startDate,
+          endDate: prestamo.endDate
+        }
+        this.setState({ prestamos: this.state.prestamos.concat([data]) })
+      })
     })
-    fetch('http://localhost:3001/users/'+this.props.userId)
-    .then(function(res) {
-      this.setState({usuario : res.data})
-    })
+    console.log(this.state.prestamos)
   }
 
-
   render() { 
-    const {prestamoId, paymentId, objectId, userId, fechaI, fechaF, } = this.props
+    const {prestamos } = this.state
     return (
+
       <div>
-        <h2>Prestamos usuario {userId}</h2>
-        <table>
-            <tr>
-                <th>Id Prestamo</th>
-                <th>Fecha Inicio</th>
-                <th>Fecha Fin</th>
-            </tr>
-            <tr>
-                <td>{prestamoId}</td>
-                <td>{paymentId}</td>
-                <td>{objectId}</td>
-                <td>{fechaI}</td>
-                <td>{fechaF}</td>
-            </tr>
-        </table>
+        <h1>{this.state.usuario.name} loans</h1>
+        <div className="container">
+              <div className="row">
+          {prestamos.map((prestamo,index)=>(
+            <Fragment>
+              <div className="col-auto mb-3">
+              <div className="card" style={{width: "25rem"}}>
+                <div className="card-body">
+                  <h2 className="card-title">Loan {prestamo.id} </h2>
+                    <p className="prestamoT" style={{fontSize: "1.8rem"}}>
+                    paymentId: {prestamo.paymentId} <br></br>
+                    objectId: {prestamo.objectId} <br></br>
+                    Start Date: {prestamo.startDate} <br></br>
+                    End Date: {prestamo.endDate} <br></br>
+                    <a href="#" className="card-link">Update</a>
+                    <a href="#" className="card-link">Delete</a>
+                    </p>
+              </div>
+              </div>
+              </div>
+             </Fragment>
+          ))}
+          </div>
+          </div>
+          <Route
+           exact path='/prestamos/create'
+           render = { props => (
+            <CrearPrestamo>
+
+            </CrearPrestamo>
+           )
+           }
+          />
+          <Link to='/prestamos/create' style={{fontSize: "1.8rem"}}>Create a Loan!</Link>
       </div>
     )
   }
