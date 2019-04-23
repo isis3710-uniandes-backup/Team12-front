05ddd2d47c18169535bdb08fc80e0c7b54cf13b4
DESKTOP\User <jp.monsalvo@uniandes.edu.co>
2019-04-22 23:11:01 -0500

    Ofertas Inglés

--- front/src/components/crearOferta.js
+++ front/src/components/crearOferta.js
@@ -1,54 +1,55 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { FormattedMessage } from 'react-intl';
 
 export class crearOferta extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       nombre: "",
       img: "",
       precioAnterior: "",
-      porcentajeDescuento : "",
+      porcentajeDescuento: "",
       objetosId: "",
       usuario: JSON.parse(localStorage.getItem("user")),
       oferta: {}
+    }
   }
-}
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
-  
-  crear = (oferta)=> {
+
+  crear = (oferta) => {
     console.log(oferta)
-      axios.post(`http://localhost:3001/users/${this.state.usuario.id}/ofertas`, oferta)
+    axios.post(`http://localhost:3001/users/${this.state.usuario.id}/ofertas`, oferta)
   }
 
   render() {
     console.log(this.state.usuario.id)
     return (
       <div>
-      <div className="container">
-      <h2>Create a loan</h2>
-      
-       <form onSubmit={()=>this.crear({ paymentId: this.state.paymentId, objectId: this.state.objectId, userId:this.state.usuario.id, startDate: this.state.startDate, endDate: this.state.endDate})}>
-       <div className="form-group">
-            <label htmlFor="paymentId">Payment id: </label>
-            <input name="paymentId" value={this.state.paymentId} className="form-control" id="paymentId" placeholder="Insert the payment id" onChange={this.onChange}/>
-        </div>
-        <div className="form-group">
-            <label htmlFor="objectId">Object id: </label>
-            <input name="objectId" value={this.state.objectId} className="form-control" id="objectId" placeholder="Insert the object id" onChange={this.onChange}/>
-        </div>
-        <div className="form-group">
-            <label htmlFor="startDate">Start Date: </label>
-            <input name="startDate" value={this.state.startDate} className="form-control" id="startDate" placeholder="dd/mm/aa" onChange={this.onChange}/>
-        </div>
-        <div className="form-group">
-            <label htmlFor="endDate">End Date: </label>
-            <input name="endDate" value={this.state.endDate} className="form-control" id="endDate" placeholder="dd/mm/aa" onChange={this.onChange}/>
-        </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
-        </form> 
+        <div className="container">
+          <h2><FormattedMessage id="ofertas.create" /></h2>
+
+          <form onSubmit={() => this.crear({ paymentId: this.state.paymentId, objectId: this.state.objectId, userId: this.state.usuario.id, startDate: this.state.startDate, endDate: this.state.endDate })}>
+            <div className="form-group">
+              <label htmlFor="paymentId"><FormattedMessage id="prestamos.pid" /> </label>
+              <input name="paymentId" value={this.state.paymentId} className="form-control" id="paymentId" placeholder="Insert the payment id" onChange={this.onChange} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="objectId"><FormattedMessage id="prestamos.oid" /> </label>
+              <input name="objectId" value={this.state.objectId} className="form-control" id="objectId" placeholder="Insert the object id" onChange={this.onChange} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="startDate"><FormattedMessage id="prestamos.sd" /> </label>
+              <input name="startDate" value={this.state.startDate} className="form-control" id="startDate" placeholder="dd/mm/aa" onChange={this.onChange} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="endDate"><FormattedMessage id="prestamos.ed" /> </label>
+              <input name="endDate" value={this.state.endDate} className="form-control" id="endDate" placeholder="dd/mm/aa" onChange={this.onChange} />
+            </div>
+            <button type="submit" className="btn btn-primary"><FormattedMessage id="prestamos.submit" /></button>
+          </form>
         </div>
       </div>
     )
@@ -56,4 +57,3 @@ export class crearOferta extends Component {
 }
 
 export default crearOferta
- 
--- front/src/components/oferta.js
+++ front/src/components/oferta.js
@@ -1,109 +1,110 @@
-import React, { Component,Fragment } from 'react'
-import {Link, Route} from 'react-router-dom'
+import React, { Component, Fragment } from 'react'
+import { Link, Route } from 'react-router-dom'
 import CrearOferta from './crearOferta'
 import UpdateOferta from './updateOferta'
 import axios from 'axios';
 import ApiHelper from './ApiHelper';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { FormattedMessage } from 'react-intl';
 
 export class oferta extends Component {
 
   api = new ApiHelper();
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      ofertas : [],
+      ofertas: [],
       usuario: JSON.parse(localStorage.getItem("user"))
     };
   }
-  
+
   componentDidMount = () => {
-    if(this.api.loggedIn()){
+    if (this.api.loggedIn()) {
       fetch(`http://localhost:3001/users/${this.state.usuario.id}/ofertas`)
-      .then(response => response.json())
-      .then(ofertas => {
-        ofertas.forEach(oferta => {
-          let data = {
-            id: oferta.id,
-            nombre: oferta.nombre,
-            img: oferta.img,
-            precioAnterior: oferta.precioAnterior,
-            porcentajeDescuento: oferta.porcentajeDescuento,
-            objetosId: oferta.objectId
-          }
-          this.setState({ ofertas: this.state.ofertas.concat([data]) })
+        .then(response => response.json())
+        .then(ofertas => {
+          ofertas.forEach(oferta => {
+            let data = {
+              id: oferta.id,
+              nombre: oferta.nombre,
+              img: oferta.img,
+              precioAnterior: oferta.precioAnterior,
+              porcentajeDescuento: oferta.porcentajeDescuento,
+              objetosId: oferta.objectId
+            }
+            this.setState({ ofertas: this.state.ofertas.concat([data]) })
+          })
         })
-      })
       console.log(this.state.ofertas)
     }
-    
+
   }
 
-  delete = (id)=>{
+  delete = (id) => {
     console.log(id)
-    axios.delete(`http://localhost:3001/users/${this.state.usuario.id}/ofertas/`+id)
-    .then(res =>{
-      this.setState({ofertas: [...this.state.ofertas.filter(oferta=> oferta.id!==id)]})
-    })
+    axios.delete(`http://localhost:3001/users/${this.state.usuario.id}/ofertas/` + id)
+      .then(res => {
+        this.setState({ ofertas: [...this.state.ofertas.filter(oferta => oferta.id !== id)] })
+      })
   }
 
-  render() { 
-    if(this.api.loggedIn()){
-      const {ofertas } = this.state
+  render() {
+    if (this.api.loggedIn()) {
+      const { ofertas } = this.state
       return (
 
         <div>
-          <h1>{this.state.usuario.name} offers</h1>
+          <h1>{this.state.usuario.name + ":"} <FormattedMessage id="ofertas" /></h1>
           <div className="container">
-                <div className="row">
-            {ofertas.map((oferta,index)=>(
-              <Fragment>
-                <div key={index} className="col-auto mb-3">
-                  <div className="card" style={{width: "25rem"}}>
-                    <div className="card-body">
-                      <h2 className="card-title"> {oferta.nombre} </h2>
-                        <p className="prestamoT" style={{fontSize: "1.8rem"}}>
-                        <img src={oferta.img} width="200px" height="200px" alt={oferta.nombre} />
-                        name: {oferta.nombre} <br></br>
-                        previous price: {"$"+oferta.precioAnterior} {"- "+oferta.porcentajeDescuento+"%"} <br></br>
-                        current price: {"$"+oferta.precioAnterior*(oferta.porcentajeDescuento/100)} <br></br>
-                        <button className="btn btn-danger" style={{fontSize: "1.5rem"}} onClick={()=> this.delete(oferta.id)}>Delete </button>
-                        <Link className="btn btn-warning" to={'/ofertas/update/'+oferta.id} style={{fontSize: "1.5rem"}}>Update </Link>
-                          </p>
+            <div className="row">
+              {ofertas.map((oferta, index) => (
+                <Fragment>
+                  <div key={index} className="col-auto mb-3">
+                    <div className="card" style={{ width: "25rem" }}>
+                      <div className="card-body">
+                        <h2 className="card-title"> {oferta.nombre} </h2>
+                        <p className="prestamoT" style={{ fontSize: "1.8rem" }}>
+                          <img src={oferta.img} width="200px" height="200px" alt={oferta.nombre} />
+                          <FormattedMessage id="tItemName" />: {oferta.nombre} <br></br>
+                          <FormattedMessage id="ofertas.prevp" />{"$" + oferta.precioAnterior} {"- " + oferta.porcentajeDescuento + "%"} <br></br>
+                          <FormattedMessage id="ofertas.currp" />{"$" + oferta.precioAnterior * (oferta.porcentajeDescuento / 100)} <br></br>
+                          <button className="btn btn-danger" style={{ fontSize: "1.5rem" }} onClick={() => this.delete(oferta.id)}><FormattedMessage id="prestamos.delete" /> </button>
+                          <Link className="btn btn-warning" to={'/ofertas/update/' + oferta.id} style={{ fontSize: "1.5rem" }}><FormattedMessage id="prestamos.update" /> </Link>
+                        </p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </Fragment>
-            ))}
+                </Fragment>
+              ))}
             </div>
-            </div>
-            <Route
+          </div>
+          <Route
             exact path='/ofertas/create'
-            render = { props => (
+            render={props => (
               <CrearOferta>
 
               </CrearOferta>
             )
             }
-            />
-            <Route
-                      exact path='/ofertas/update/:id' render = { props => (
-                        
-                        <UpdateOferta {...props}>
-              
-                        </UpdateOferta>
-                      )
-                      }
-                      />
-            <Link to='/ofertas/create' style={{fontSize: "1.8rem"}}>Create an offer!</Link>
+          />
+          <Route
+            exact path='/ofertas/update/:id' render={props => (
+
+              <UpdateOferta {...props}>
+
+              </UpdateOferta>
+            )
+            }
+          />
+          <Link to='/ofertas/create' style={{ fontSize: "1.8rem" }}><FormattedMessage id="ofertas.create" /></Link>
         </div>
       )
     }
-    else{
+    else {
       return (
         <div>
-          <p style={{textAlign:"center", fontSize:"3em", color:"black"}}>No estás registrado</p>
-          <p style={{textAlign:"center"}}><NavLink to="/signup">Regístrate ahora</NavLink> para obtener ofertas personalizadas</p>
+          <p style={{ textAlign: "center", fontSize: "3em", color: "black" }}>No estás registrado</p>
+          <p style={{ textAlign: "center" }}><NavLink to="/signup">Regístrate ahora</NavLink> para obtener ofertas personalizadas</p>
         </div>
       );
     }
